825e3e9ddbb4b57b6a0dfc54e69086bf52653bd9
Kristijan Stefanac <kstefanac20@gmail.com>
2018-10-05 00:33:05 +0200

    ask for password

---
+++ askPassword.py
@@ -0,0 +1,8 @@
+correctPass = "p1"
+
+password = input("Please enter your password: ")
+
+while correctPass != password:
+	password = input("Please enter correct password: ")
+
+print("Login succesfull")
